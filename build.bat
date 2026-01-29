e03ca2037031179a86fd65ef80b4c34ad9dff0c4
Mohamed Abdifataax Jama <mahdiware05@gmail.com>
2026-01-29 11:47:38 +0300

    Simplify build.bat and remove shared library option
    Refactor build.bat to simplify build process and remove unused options.

--- build.bat
+++ build.bat
@@ -11,6 +11,7 @@ set "project_root=%~dp0"
 :: ----------------------------------------------------------------------------
 :: DEPENDENCIES
 :: ----------------------------------------------------------------------------
+:: These paths are populated by the GitHub Action PowerShell step
 set "pcre2_path=%project_root%deps\pcre2"
 set "pcre2_inc=/I"%pcre2_path%\include""
 set "pcre2_lib="%pcre2_path%\lib\pcre2-8-static.lib""
@@ -19,25 +20,11 @@ set "pcre2_lib="%pcre2_path%\lib\pcre2-8-static.lib""
 :: PARSE COMMAND LINE ARGUMENTS
 :: ----------------------------------------------------------------------------
 set "enable_debug=true"
-set "use_shared_lib=false"
 
 :PARSE_ARGS
 if "%~1"=="" goto :CHECK_MSVC
-if "%~1"=="-h" goto :PRINT_USAGE
-if "%~1"=="-c" goto :CLEAN
 if "%~1"=="-r" (set "enable_debug=false" & shift & goto :PARSE_ARGS)
-if "%~1"=="-s" (set "use_shared_lib=true" & shift & goto :PARSE_ARGS)
-
-echo Invalid argument "%~1"
-
-:PRINT_USAGE
-echo Usage: call build.bat [options ...]
-echo options:
-echo   -h  display this message
-echo   -r  Compile the release version of saynaa (default = debug)
-echo   -s  Link saynaa as a shared library (default = static link).
-echo   -c  Clean all compiled/generated intermediate binaries.
-goto :END
+if "%~1"=="-c" goto :CLEAN
 
 :: ----------------------------------------------------------------------------
 :: INITIALIZE MSVC ENVIRONMENT
@@ -47,51 +34,34 @@ if defined INCLUDE goto :START
 
 echo Not running on an MSVC prompt, searching for one...
 
-:: Find vswhere.exe safely
 set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 if not exist "!vswhere!" set "vswhere=%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
 
 if not exist "!vswhere!" (
     echo Error: can't find vswhere.exe
-    goto :NO_VS_PROMPT
+    exit /b 1
 )
 
-:: Get the installation path using vswhere
 for /f "usebackq tokens=*" %%i in (`"!vswhere!" -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
     set "install_path=%%i"
 )
 
-if not exist "!install_path!" (
-    echo Error: can't find Visual Studio installation directory
-    goto :NO_VS_PROMPT
-)
-
-echo Found at - "!install_path!"
-
-:: Call vcvars64.bat to set up the environment
 set "vcvars=!install_path!\VC\Auxiliary\Build\vcvars64.bat"
 if not exist "!vcvars!" (
     echo Error: can't find vcvars64.bat
-    goto :NO_VS_PROMPT
+    exit /b 1
 )
 
 call "!vcvars!"
-if errorlevel 1 goto :NO_VS_PROMPT
-echo Initialized MSVC x86_64
-goto :START
-
-:NO_VS_PROMPT
-echo You must open a "Visual Studio .NET Command Prompt" to run this script or ensure VS is installed.
-exit /b 1
+if errorlevel 1 exit /b 1
 
 :: ----------------------------------------------------------------------------
 :: START BUILD
 :: ----------------------------------------------------------------------------
 :START
 set "target_dir=%project_root%obj\"
-set "add_cflags=-W3 -GR /FS -EHsc"
-set "add_linkflags=/SUBSYSTEM:CONSOLE"
 set "add_defines=/D_CRT_SECURE_NO_WARNINGS /DPCRE2_STATIC"
+set "add_cflags=-W3 -GR /FS -EHsc"
 
 if "!enable_debug!"=="false" (
     set "cflags=-O2 -MD /DNDEBUG"
@@ -100,19 +70,12 @@ if "!enable_debug!"=="false" (
     set "add_defines=!add_defines! /DDEBUG"
 )
 
-if "!use_shared_lib!"=="true" (
-    set "add_defines=!add_defines! /D_DLL_ /D_COMPILE_"
-)
-
 :: Create directories
-if not exist "!target_dir!" mkdir "!target_dir!"
-if not exist "!target_dir!lib\" mkdir "!target_dir!lib\"
 if not exist "!target_dir!saynaa\" mkdir "!target_dir!saynaa\"
 if not exist "!target_dir!cli\" mkdir "!target_dir!cli\"
+if not exist "!target_dir!lib\" mkdir "!target_dir!lib\"
 
-:: ----------------------------------------------------------------------------
-:: COMPILE CORE
-:: ----------------------------------------------------------------------------
+:: 1. Compile Core (Compiler, VM, Optionals)
 cd /d "!target_dir!saynaa"
 cl /nologo /c !add_defines! !pcre2_inc! !add_cflags! !cflags! ^
     "!project_root!src\compiler\*.c" ^
@@ -122,36 +85,27 @@ cl /nologo /c !add_defines! !pcre2_inc! !add_cflags! !cflags! ^
     "!project_root!src\utils\*.c"
 if errorlevel 1 goto :FAIL
 
+:: 2. Create Library
 set "mylib=!target_dir!lib\saynaa.lib"
-if "!use_shared_lib!"=="true" (
-    set "mylib=!target_dir!bin\saynaa.lib"
-    if not exist "!target_dir!bin\" mkdir "!target_dir!bin\"
-    link /nologo /dll /out:"!target_dir!bin\saynaa.dll" /implib:"!mylib!" *.obj !pcre2_lib!
-) else (
-    lib /nologo !add_linkflags! /OUT:"!mylib!" *.obj
-)
+lib /nologo /OUT:"!mylib!" *.obj
 if errorlevel 1 goto :FAIL
 
-:: ----------------------------------------------------------------------------
-:: COMPILE CLI
-:: ----------------------------------------------------------------------------
+:: 3. Compile CLI
 cd /d "!target_dir!cli"
 cl /nologo /c !add_defines! !pcre2_inc! !add_cflags! !cflags! "!project_root!src\cli\*.c"
 if errorlevel 1 goto :FAIL
 
-:: Final Link
+:: 4. Final Link to Executable
 cd /d "!project_root!"
 cl /nologo !add_defines! "!target_dir!cli\*.obj" "!mylib!" !pcre2_lib! /Fe"saynaa.exe"
 if errorlevel 1 goto :FAIL
 
-echo.
-echo Compilation Success: saynaa.exe is ready.
+echo Build Successful: saynaa.exe created.
 goto :END
 
 :CLEAN
 if exist "obj" rmdir /S /Q "obj"
 if exist "saynaa.exe" del "saynaa.exe"
-echo Files were cleaned.
 goto :END
 
 :FAIL
